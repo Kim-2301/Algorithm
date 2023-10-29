582c0815c0133a84d73f89d8ad94d23205f97e1f
Kim-2301 <seunghhhh2344@gmail.com>
2023-10-29 21:01:49 +0900

    02 solution

---
+++ 0X00/00_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int func2(int arr[], int N){
+    int result=0;
+
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            if(arr[i]+arr[j]==100){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(void){
+    int arr[]={};
+    int N=0;
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+
+    returnfunc2(arr,N);
+
+}
