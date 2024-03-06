96b5315d70fe9fffb233da3e4aa0b9cdd2670dfe
Kim-2301 <seunghhhh2344@gmail.com>
2024-03-06 23:23:54 +0900

    #9095

---
+++ 동적계획법/#9095.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    int dp[12];
+  
+    dp[1]=1;
+    dp[2]=2;
+    dp[3]=4;
+
+    for(int i=4; i<12; i++){
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+    }
+
+    while(t--){
+        int n;
+        cin >> n;
+        cout << dp[n]<<'\n';
+
+    }
+
+
+}
+
+/*
+dp[1] = 1 => 1
+dp[2] = 1+1, 2 => 2
+dp[3] = 1+1+1, 1+2, 2+1, 3 => 4
+dp[4] = 1+1+1+1, 1+1+2, 1+2+1, 1+3, 2+1+1, 2+2, 3+1 =>7
+dp[n] = dp[n-1] + dp[n-2] + dp[n-3]
+*/
