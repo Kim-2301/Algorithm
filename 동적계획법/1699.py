5c909e30a6d3b276766f5582e947493572571071
Kim-2301 <seunghhhh2344@gmail.com>
2024-03-08 23:51:02 +0900

    #1699

---
+++ 동적계획법/1699.py
@@ -0,0 +1,11 @@
+import math
+N = int(input())
+dp = [x for x in range(N+1)]
+
+
+for i in range(1, N + 1):
+    for j in range(1, int(math.sqrt(i) + 1)):
+        if dp[i] > (dp[i - j * j] + 1):
+            dp[i] = dp[i - j * j] + 1
+        
+print(dp[N])
